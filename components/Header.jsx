4b739f8d26040ab3001cf611584b8d60a15bb16b
Rodrigo <rodaragaoandrade@gmail.com>
2026-09-24 23:21:43 -0300

    Reduce header top padding a bit further

--- components/Header.jsx
+++ components/Header.jsx
@@ -23,7 +23,7 @@ export function Header({ streak, onNavigate, missionDone }) {
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
-        padding: "10px 24px 6px",
+        padding: "6px 24px 6px",
         background: "transparent",
       }}
     >
